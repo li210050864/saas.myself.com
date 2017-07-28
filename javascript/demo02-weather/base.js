2d6f3153b212b28d023cda6391ceef522bb6ef72
li210050864 <li210050864@163.com>
2017-07-28 17:44:41 +0800

    weather js

---
+++ javascript/demo02-weather/base.js
@@ -0,0 +1,63 @@
+function daysObj(){
+    var todayObj = new Date(),
+        yesterdayObj = new Date(),
+        weekdays = [];
+    year = function(dateObj){
+        return dateObj.getFullYear();
+    },
+    month = function(dateObj){
+        return dateObj.getMonth() + 1;
+    },
+    date = function(dateObj){
+        return dateObj.getDate();
+    },
+    week = function(dateObj){
+        var weekstr = "星期",
+            weekday = dateObj.getDay(),
+            weekval = ["日","一","二","三","四","五","六"];
+       if(weekday && typeof weekday == "Number"){
+           weekstr += weekval[weekday];
+       }
+    },
+    getToday = function(){
+        return month(todayObj) + "月" + date(todayObj) + "日";
+    },
+    getYeaterday = function(){
+        yesterdayObj.setTime(todayObj.getTime() - 3600 * 24 * 1000);
+        return month(yesterdayObj) + "月" + date(yesterdayObj) + "日";
+    },
+    getWeekdays = function(){
+        var dateStr ="";
+        for(var i = 6; i >= 0; i--){
+            dateStr = "";
+            var tmpdateObj = new Date();
+                tmpdateObj.setTime(todayObj.getTime() - 3600 * 24 * 1000 * i);
+            dateStr = month(tmpdateObj) + "月" + date(tmpdateObj) + "日";
+            weekdays.push(dateStr);
+        }
+        dateStr = getYeaterday();
+        weekdays.push(dateStr);
+        return weekdays;
+    };
+    /**
+     * 获取指定月份的天数
+     * @param year 表示年份的四位数字
+     * @param month 表示月份的整数 介于 0 ~ 11
+     * tip： UTC 方法可根据世界时返回 1970 年 1 月 1 日 到指定日期的毫秒数
+     *       getUTCDate： 方法可根据世界时返回一个月 (UTC) 中的某一天。
+     * @returns {number}
+     */
+    function daysInMonth(year,month){
+        return new Date(Date.UTC(year,month+1,0)).getUTCDate();
+    }
+    function getDaysInMonth(){
+        return daysInMonth(year(todayObj),month(todayObj));
+    }
+    return {getDaysInMonth:getDaysInMonth,getWeekdays:getWeekdays}
+}
+(function($){
+    var date = new daysObj();
+    dateVal = date.getDaysInMonth();
+    var weekdays = date.getWeekdays();
+    console.log(weekdays);
+})($);
---
+++ javascript/demo02-weather/iplook.php
@@ -0,0 +1,54 @@
+<?php
+function get_ip(){
+    $realip = "";
+    $unknown = "unknown";
+    if(isset($_SERVER)){
+        if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown)){
+            $realip = $_SERVER['HTTP_CLIENT_IP'];
+        }else if(isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR']) && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown)){
+            $realip = $_SERVER['REMOTE_ADDR'];
+        }else{
+            $realip = $unknown;
+        }
+    }else{
+        if(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), $unknown)){
+            $realip = getenv("HTTP_X_FORWARDED_FOR");
+        }else if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), $unknown)){
+            $realip = getenv("HTTP_CLIENT_IP");
+        }else if(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), $unknown)){
+            $realip = getenv("REMOTE_ADDR");
+        }else{
+            $realip = $unknown;
+        }
+    }
+    $realip = preg_match("/[\d\.]{7,15}/", $realip, $matches) ? $matches[0] : $unknown;
+    return $realip;
+}
+function get_ip_lookup($ip){
+    if(empty($ip)){
+        $ip = $this -> get_ip();
+    }
+    $res = @file_get_contents('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip=' . $ip);
+    if(empty($res)){
+        return false;
+    }
+    $jsonMatches = array();
+    preg_match('#\{.+?\}#',$res,$jsonMatches);
+    if(!isset($jsonMatches[0])){
+        return false;
+    }
+    $json = json_decode($jsonMatches[0],true);
+    var_dump($json);
+    if(isset($json['ret']) && $json['ret'] == 1){
+        $json['ip'] = $ip;
+        unset($json['ret']);
+    }else{
+        return false;
+    }
+    return $json;
+}
+
+// $curip = get_ip();
+// $location = get_ip_lookup($curip);
+// echo $location;
+?>
